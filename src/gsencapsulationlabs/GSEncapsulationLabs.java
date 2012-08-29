4048e690e7164b58bf18ea09be5d69057d8c8ea7
gsingh8 <gsingh8@L11703038599.wctc.msft>
2012-08-29 18:11:30 -0500

    Minor changes

--- src/gsencapsulationlabs/GSEncapsulationLabs.java
+++ src/gsencapsulationlabs/GSEncapsulationLabs.java
@@ -16,5 +16,6 @@ public class GSEncapsulationLabs {
     public static void main(String[] args) {
         // TODO code application logic here
        System.out.println("Hello World");
+        System.out.println("Second line");
     }
 }
