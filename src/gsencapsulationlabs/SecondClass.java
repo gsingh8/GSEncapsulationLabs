b2f731e5a7211940744bd0037c1e7e89cce53da2
gsingh8 <gsingh8@L11703038599.wctc.msft>
2012-08-29 18:23:58 -0500

    asdf

---
+++ src/gsencapsulationlabs/SecondClass.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gsencapsulationlabs;
+
+/**
+ *
+ * @author gsingh8
+ */
+public class SecondClass {
+    
+}
